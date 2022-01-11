634d2751d74506544b52229b1c198306049ed406
Aldo Suhartono Putra <aldhosutra@Aldos-MacBook-Pro.local>
2022-01-11 20:57:06 +0700

    improve splashscreen brands

--- android/app/src/main/java/com/enevti/app/MainActivity.java
+++ android/app/src/main/java/com/enevti/app/MainActivity.java
@@ -3,15 +3,29 @@ package com.enevti.app;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
 import android.os.Bundle;
+import android.content.res.Configuration;
 
 public class MainActivity extends ReactActivity {
 
   // SplashActivity
   @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);
-        super.onCreate(savedInstanceState);
+  protected void onCreate(Bundle savedInstanceState) {
+    switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
+        case Configuration.UI_MODE_NIGHT_YES:
+            setTheme(R.style.DarkTheme);
+            SplashScreen.show(this, R.style.DarkTheme, false);
+            break;
+        case Configuration.UI_MODE_NIGHT_NO:
+            setTheme(R.style.LightTheme);
+            SplashScreen.show(this, R.style.LightTheme, false);
+            break;
+        default:
+            setTheme(R.style.LightTheme);
+            SplashScreen.show(this, R.style.LightTheme, false);
     }
+
+    super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
--- android/app/src/main/java/com/enevti/app/SplashActivity.java
+++ android/app/src/main/java/com/enevti/app/SplashActivity.java
@@ -3,6 +3,8 @@ package com.enevti.app;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.os.Build;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
