6ed2cfd243bb15ed177f5d3012514fd770a4a77e
Aldo Suhartono Putra <aldhosutra@gmail.com>
2022-03-23 18:16:05 +0700

    fix ios header height

--- src/service/enevti/profile.ts
+++ src/service/enevti/profile.ts
@@ -22,11 +22,11 @@ async function fetchProfile(address: string): Promise<Profile | undefined> {
   const onsaleNFT = [];
   const mintedContainer: any[] = [];
 
-  for (let i = 0; i < 7; i++) {
+  for (let i = 0; i < 30; i++) {
     ownedNFT.push(getDummyNFTData());
   }
 
-  for (let i = 0; i < 7; i++) {
+  for (let i = 0; i < 30; i++) {
     onsaleNFT.push(getDummyNFTData());
   }
 
