61d37cc8c2f4a79d84ab9589e27b051b0abab24d
Aldo Suhartono Putra <aldhosutra@gmail.com>
2022-03-25 20:08:09 +0700

    improve profile tab performance

--- src/components/organism/profile/tabs/OwnedNFTComponent.tsx
+++ src/components/organism/profile/tabs/OwnedNFTComponent.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Platform, RefreshControl } from 'react-native';
+import { Platform, RefreshControl, StyleSheet } from 'react-native';
 import Animated from 'react-native-reanimated';
 import { FlatGrid, FlatGridProps } from 'react-native-super-grid';
 import { NFTBase } from '../../../../types/nft';
 import { PROFILE_HEADER_HEIGHT_PERCENTAGE } from '../AppProfileHeader';
 import { TOP_TABBAR_HEIGHT_PERCENTAGE } from '../../../atoms/view/AppTopTabBar';
-import { hp, wp } from '../../../../utils/imageRatio';
+import { hp, SafeAreaInsets, wp } from '../../../../utils/imageRatio';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import AppNFTRenderer from '../../../molecules/nft/AppNFTRenderer';
 
@@ -33,18 +33,54 @@ function Component(
   ref: any,
 ) {
   const insets = useSafeAreaInsets();
-  const noDisplay = 'none';
   const mounted = React.useRef<boolean>(false);
   const [displayed, setDisplayed] = React.useState<boolean>(false);
   const [refreshing, setRefreshing] = React.useState<boolean>(false);
 
-  const handleRefresh = () => {
+  const styles = React.useMemo(
+    () => makeStyle(insets, headerHeight, displayed),
+    [insets, headerHeight, displayed],
+  );
+  const isScrollEnabled = React.useMemo(
+    () => (refreshing ? false : scrollEnabled),
+    [refreshing, scrollEnabled],
+  );
+  const spacing = React.useMemo(() => wp('0.5%', insets), [insets]);
+  const itemDimension = React.useMemo(() => wp('30%', insets), [insets]);
+  const progressViewOffset = React.useMemo(
+    () =>
+      Platform.OS === 'ios'
+        ? headerHeight
+        : hp(
+            PROFILE_HEADER_HEIGHT_PERCENTAGE + TOP_TABBAR_HEIGHT_PERCENTAGE,
+            insets,
+          ) + headerHeight,
+    [headerHeight, insets],
+  );
+
+  const handleRefresh = React.useCallback(() => {
     setRefreshing(true);
     onRefresh && onRefresh();
     if (mounted.current) {
       setRefreshing(false);
     }
-  };
+  }, [onRefresh]);
+
+  const refreshControl = React.useMemo(
+    () => (
+      <RefreshControl
+        refreshing={false}
+        onRefresh={handleRefresh}
+        progressViewOffset={progressViewOffset}
+      />
+    ),
+    [handleRefresh, progressViewOffset],
+  );
+
+  const renderItem = React.useCallback(
+    ({ item }) => <AppNFTRenderer nft={item} width={itemDimension} />,
+    [itemDimension],
+  );
 
   React.useEffect(() => {
     if (ref && ref.current) {
@@ -61,43 +97,34 @@ function Component(
     <AnimatedFlatGrid
       ref={ref}
       onScroll={onScroll}
-      scrollEnabled={refreshing ? false : scrollEnabled}
+      scrollEnabled={isScrollEnabled}
       scrollEventThrottle={16}
-      contentContainerStyle={[
-        {
-          paddingTop:
-            hp(
-              PROFILE_HEADER_HEIGHT_PERCENTAGE + TOP_TABBAR_HEIGHT_PERCENTAGE,
-              insets,
-            ) + headerHeight,
-        },
-        { display: displayed ? undefined : noDisplay },
-      ]}
-      spacing={wp('0.5%', insets)}
+      contentContainerStyle={styles.contentContainerStyle}
+      spacing={spacing}
       showsVerticalScrollIndicator={false}
-      itemDimension={wp('30%', insets)}
+      itemDimension={itemDimension}
       data={data}
-      renderItem={({ item }) => (
-        <AppNFTRenderer nft={item} width={wp('30%', insets)} />
-      )}
-      refreshControl={
-        <RefreshControl
-          refreshing={false}
-          onRefresh={handleRefresh}
-          progressViewOffset={
-            Platform.OS === 'ios'
-              ? headerHeight
-              : hp(
-                  PROFILE_HEADER_HEIGHT_PERCENTAGE +
-                    TOP_TABBAR_HEIGHT_PERCENTAGE,
-                  insets,
-                ) + headerHeight
-          }
-        />
-      }
+      renderItem={renderItem}
+      refreshControl={refreshControl}
     />
   );
 }
+
+const makeStyle = (
+  insets: SafeAreaInsets,
+  headerHeight: number,
+  displayed: boolean,
+) =>
+  StyleSheet.create({
+    contentContainerStyle: {
+      paddingTop:
+        hp(
+          PROFILE_HEADER_HEIGHT_PERCENTAGE + TOP_TABBAR_HEIGHT_PERCENTAGE,
+          insets,
+        ) + headerHeight,
+      display: displayed ? undefined : 'none',
+    },
+  });
 
 const OwnedNFTComponent = React.forwardRef(Component);
 export default OwnedNFTComponent;
